4d3cc31e8b3a2d276705ee06aab03ce25ff344ff
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-28 02:50:17 +0530

    Create explore.js

---
+++ src/explore.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Explore extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+
+      componentDidMount() {
+      }
+      render() {
+        return(
+          <div>
+              Explore
+          </div>
+              )
+      }
+}
+export default Explore;

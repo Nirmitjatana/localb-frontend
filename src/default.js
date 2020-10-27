fd75e85a3392968dbfaf2ade25a422dceda9b3e6
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-28 02:50:14 +0530

    Create default.js

---
+++ src/default.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+
+      componentDidMount() {
+      }
+      render() {
+        return(
+          <div></div>
+              )
+      }
+}
+export default Navbar;

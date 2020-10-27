a94b1755fb267f882979707ec34c9fc266f9493d
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-28 03:22:21 +0530

    Create faq.js

---
+++ src/faq.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class FAQ extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+
+      componentDidMount() {
+      }
+      render() {
+        return(
+          <div>
+              FAQ
+          </div>
+              )
+      }
+}
+export default FAQ;

89c06233b10eb4baa43610fc10e4d4a8a3600d4c
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-28 11:26:01 +0530

    extra cards added

--- src/explore.js
+++ src/explore.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import location from './Assets/location.png'
+import sample from './Assets/sample.png'
 
 class Explore extends React.Component{
     constructor(props) {
@@ -22,12 +23,80 @@ class Explore extends React.Component{
               <h2>Near You</h2>
               <h4 className="subtext">Find and explore local businesses near you.</h4>
               <div className="search">
-                searchbox
+                <input type="text" placeholder="Search" name="Search1" className="searchbox"/>
+              </div>
+              <div className="cards">
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
               </div>
               <h2>All Listings</h2>
               <h4 className="subtext">Find the local business you are looking for.</h4>
               <div className="search">
-                searchbox
+              <input type="text" placeholder="Search" name="Search2" className="searchbox"/>
+              </div>
+              <div className="cards">
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
+              <div className="card">
+                <img src={sample} alt="businessimage" className="image"/>
+                <div>
+                <h1 className="businesshead">Cooper's kitchen</h1>
+                <h4 className="typetext">Restaurant</h4>
+                <h6 className="subtitle">An authentic fine-dining restaurant serving variations of North-American fast food</h6>
+                </div>
+              </div>
               </div>
           </div>
               )
